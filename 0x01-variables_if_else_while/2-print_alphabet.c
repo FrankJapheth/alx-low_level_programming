088c2384d1447beca41ba5c2f3d551f2ed414b8f
Okwau Franklyne Adede <adedefranklyne@gmail.com>
2023-09-10 23:55:00 -0700

    fix 2-print-alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	char alphabet[28] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'e',
-		'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+		'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
 		'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	unsigned long int i;
 
